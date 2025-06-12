ece6af28b40fc001b16056847f70f44ece82f7d2
Alena <lenazolot11@gmail.com>
2025-06-12 17:30:04 +0300

    hw9

---
+++ hw9/hw9.js
@@ -0,0 +1,82 @@
+/*const str1='Hello';
+const str2 = 'world!';
+console.log(str1 +','+ str2);*/
+
+
+/*let str = 'I learn computer science!';
+const arr = str.split(' ');
+str = arr.join ('_');
+console.log(str);*/
+
+
+/*let str = 'I learn computer science!';
+const arr = str.split('');
+console.log(length);
+console.log(arr);*/
+
+
+/*
+let str = 'я учусь дома';
+const arr = str.split('');
+arr[0] = arr[0].toUpperCase();
+arr[2] = arr[2].toUpperCase();
+arr[4] = arr[4].toUpperCase();
+arr[6] = arr[6].toUpperCase();
+arr[8] = arr[8].toUpperCase();
+arr[10] = arr[10].toUpperCase();
+str = arr.join('');
+console.log(str);
+*/
+
+
+/*let min = 11;
+const arr = [14,29,44,59];
+for (let i = 0; i < arr.length; i++) {
+    if (min <= arr[i]) {
+        console.log(`${i + 1}-я четверть часа`);
+        break;
+    }
+    }*/
+
+
+/*let lang = "ru";
+let days;
+if (lang === "ru") {
+    days = ['Понедельник', 'Вторник', 'Среда','Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+}
+if (lang === "en") {
+    days = ['Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday', 'Saturday', 'Sunday'];
+}
+console.log(days);*/
+
+
+/*let lang = prompt ("Введите язык (ru/en):");
+switch (lang) {
+    case "ru":
+        console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
+        break;
+    case "en":
+        console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+        break;
+    default:
+        console.log("Неверный язык");
+        break;
+}*/
+
+
+/*let user = {
+    name: "Jane",
+    age: 21
+};
+if (user.name === "Jane") {
+    console.log(`This is Jane. She is ${user.age} `);
+}*/
+
+
+
+
+
+
+
+
+
